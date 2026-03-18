8b2355a34e99956de9f464dd1c8bfd85c6df3c34
Matthew Shaw <mshaw39@charlotte.edu>
2026-03-18 17:01:02 -0400

    Update Member.cs
    Added UpdateMember method to update any member information as necessary.

--- Domain/Member.cs
+++ Domain/Member.cs
@@ -2,12 +2,18 @@
 
 public class Member
 {
-    public int Account { get; set; }
+    public int AccountId { get; set; }
     public string Name { get; set; }
 
-    public Member(int account, string name)
+    public Member(int accountId, string name)
     {
-        Account = account;
+        AccountId = accountId;
+        Name = name;
+    }
+
+    public void UpdateMember(int accountId, string name)
+    {
+        AccountId = accountId;
         Name = name;
     }
 }
