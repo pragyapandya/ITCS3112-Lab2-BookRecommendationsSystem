01a85cb03aa532bfd1aa3bc8c848c45cda9efb42
Pragya Pandya <pragyapandya.108@gmail.com>
2026-03-20 20:41:46 -0400

    Quick save for all Member work

--- Contracts/IMemberService.cs
+++ Contracts/IMemberService.cs
@@ -1,6 +1,13 @@
-﻿namespace Lab2BookRecommendationSystem.Contracts;
+﻿using Lab2BookRecommendationSystem.Domain;
+
+namespace Lab2BookRecommendationSystem.Contracts;
 
 public interface IMemberService
 {
-    
+    Member CreateNewMember(string name);
+    bool Login(int account);
+    void Logout();
+    Member GetLoggedInMember();
+    bool IsLoggedIn();
+    bool UpdateLoggedInMember(string newName); //so members can change name once logged in
 }
