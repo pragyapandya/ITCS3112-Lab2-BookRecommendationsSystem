1679ca859a86b9862722bbdba490f3e6e209b736
Pragya Pandya <pragyapandya.108@gmail.com>
2026-03-20 20:44:41 -0400

    Moving Service file to correct service branch

--- Repositories/MemberRepo.cs
+++ Repositories/MemberRepo.cs
@@ -1,6 +1,35 @@
-﻿namespace Lab2BookRecommendationSystem.Repositories;
+﻿using Lab2BookRecommendationSystem.Contracts;
+using Lab2BookRecommendationSystem.Domain;
 
-public class MemberRepo
+namespace Lab2BookRecommendationSystem.Repositories;
+
+public class MemberRepo : IMemberRepo
 {
-    
+    private readonly List<Member> _members = new List<Member>();
+
+    public void AddMember(Member member)
+    {
+        _members.Add(member);
+    }
+
+    public Member GetMemberByAccount(int account)
+    {
+        return _members.SingleOrDefault(m => m.Account == account);
+    }
+
+    public int GenerateNewAccountId()
+    {
+        if (_members.Count == 0) return 1;
+        return _members.Max(m => m.Account) + 1;
+    }
+
+    public void UpdateMember(Member updatedMember)
+    {
+        var existingMember = GetMemberByAccount(updatedMember.Account);
+     
+        if (existingMember != null)
+        {
+            existingMember.Name = updatedMember.Name;
+        }
+    }
 }
--- Services/MemberService.cs
+++ Services/MemberService.cs
@@ -1,6 +1,63 @@
-﻿namespace Lab2BookRecommendationSystem.Services;
+﻿using Lab2BookRecommendationSystem.Contracts;
+using Lab2BookRecommendationSystem.Domain;
 
-public class MemberService
+namespace Lab2BookRecommendationSystem.Services;
+
+public class MemberService : IMemberService
 {
+    private readonly IMemberRepo _repo;
+    private Member _loggedInMember;
     
+    public MemberService(IMemberRepo repo)
+    {
+        _repo = repo;
+        _loggedInMember = null;
+    }
+
+    public Member CreateNewMember(string name)
+    {
+        int newAccountID = _repo.GenerateNewAccountId();
+        Member newMember = new Member(newAccountID, name);
+        _repo.AddMember(newMember);
+        return newMember;
+    }
+
+    public bool Login(int account)
+    {
+        Member member = _repo.GetMemberByAccount(account);
+        if (member != null)
+        {
+            _loggedInMember = member;
+            return true;
+        }
+        return false;
+    }
+
+    public void Logout()
+    {
+        _loggedInMember = null;
+    }
+
+    public Member GetLoggedInMember()
+    {
+        return _loggedInMember;
+    }
+
+    public bool IsLoggedIn()
+    {
+        return _loggedInMember != null;
+    }
+
+    public bool UpdateLoggedInMember(string newName)
+    {
+        if (!IsLoggedIn() || string.IsNullOrWhiteSpace(newName))
+        {
+            return false;
+        }
+        
+        _loggedInMember.Name = newName;
+        _repo.UpdateMember((_loggedInMember));
+
+        return true;
+    }
 }
